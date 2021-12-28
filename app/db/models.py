9f467a9d0db53b68da1f2d7a0104ff061dbc48a2
Anastasiya <leloneya@gmail.com>
2021-12-28 15:22:28 +0300

    added connection to db and user model

---
+++ app/db/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+
+from app.db.database import Base
+
+
+class DbUser(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    email = Column(String)
+    password = Column(String)
