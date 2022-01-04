49de41ebfe8389d43f7335f177622aa1ded351e0
Anastasiya <leloneya@gmail.com>
2022-01-04 12:09:19 +0300

    Removed unnecessary code

--- app/main.py
+++ app/main.py
@@ -10,8 +10,3 @@ app.include_router(user.router)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
